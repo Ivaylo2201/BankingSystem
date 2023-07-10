0265b133e6e03c0356618ca33e0cd195cdd12c68
Ivaylo Georgiev <ivailo_g03@abv.bg>
2023-07-10 16:53:25 +0300

    initial commit

--- functions/view_database.py
+++ functions/view_database.py
@@ -4,7 +4,6 @@ from typing import List
 def view_database(database: dict) -> str:
     accounts_list: List[str] = []
 
-    #test
     # Keeping track of the index and the account itself and adding
     # a string containing its data along with the index
     for index, account in enumerate(database.values()):
